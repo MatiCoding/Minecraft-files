0b876c2c4a8c9e19d43372287c183e6b3d7f28af
Mati <94741657+MatiCoding@users.noreply.github.com>
2021-12-20 17:09:50 +0100

    Add files via upload

---
+++ seed minecraft.py
@@ -0,0 +1,7 @@
+# Made by MatiCoding - GitHub
+# This program is made for practising with the random library.
+# In this case I want to have a random seed for my new minecraft world and, as I am learning python, I thought it could be a nice thing to try to generate the seed with this language. 
+
+import random # I import the random library.
+
+print(f"Your seed is: {random.randint(1,999999999999999)}")   #We print the seed with the random.randint() function and the seed we get, must be between 1 and 999999999999999
